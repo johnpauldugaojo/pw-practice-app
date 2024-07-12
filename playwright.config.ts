ecd224a4b31ed5c076b7208b456b074387715242
Jayps Dugaojo <johnpauldugaojo@gmail.com>
2024-07-12 12:50:17 +0800

    new commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -82,4 +82,10 @@ export default defineConfig<TestOptions>({
       },
     },
   ],
+
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:4200/",
+    timeout: 120 * 1000,
+  },
 });
